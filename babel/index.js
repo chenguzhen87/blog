723f448f3663380cf46fa6d8f9a501027d0be00d
chenguzhen87 <546369005@qq.com>
2021-10-14 18:59:10 +0800

    feat: css3 transform

---
+++ babel/index.js
@@ -0,0 +1,16 @@
+import * as parser from "@babel/parser";
+import traverse from "@babel/traverse";
+
+const code = `function square(n) {
+  return n * n;
+}`;
+
+const ast = parser.parse(code);
+
+traverse(ast, {
+  enter(path) {
+    if (path.isIdentifier({ name: "n" })) {
+      path.node.name = "x";
+    }
+  },
+});
